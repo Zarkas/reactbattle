3e913818a12a7b1ddeb70e07c20e57a12a58727d
arkaitz.bregel <arkaitz.bregel@havasit.com>
2018-04-18 18:27:58 +0200

    Página Popular

--- app/index.js
+++ app/index.js
@@ -1,14 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
+var App = require('./component/App')
 
-class App extends React.Component {
-	render () {
-		return (
-			<div>Hello</div>
-		)
-	}
-}
 
 ReactDOM.render(
 	<App />,
